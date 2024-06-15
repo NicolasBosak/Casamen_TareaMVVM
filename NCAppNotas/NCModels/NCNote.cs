d2f0bd77db4499c30ee49e00e26a9c7815e56c07
Nicolas Casamen <sebastian.casamen@udla.edu.ec>
2024-06-15 17:40:27 -0500

    Se agrega la carpeta NVViewModel

--- NCAppNotas/NCModels/NCNote.cs
+++ NCAppNotas/NCModels/NCNote.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace NCAppNotas.NCModels
 {
@@ -11,5 +12,46 @@ namespace NCAppNotas.NCModels
         public string Filename { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+
+        public NCNote()
+        {
+            Filename = $"{Path.GetRandomFileName()}.notes.txt";
+            Date = DateTime.Now;
+            Text = "";
+        }
+
+        public void Save() =>
+        File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void Delete() =>
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static NCNote Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("No es posible encontrar esta nota en el dispositivo.", filename);
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
+
+        public static IEnumerable<NCNote> LoadAll()
+        {
+            string appDataPath = FileSystem.AppDataDirectory;
+            return Directory
+
+                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                    .Select(filename => NCNote.Load(Path.GetFileName(filename)))
+
+                    .OrderByDescending(note => note.Date);
+        }
     }
 }
---
+++ NCAppNotas/NCViewModels/NCAboutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NCAppNotas.NCViewModels
+{
+    internal class NCAboutViewModel
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "App de NOtas por Nicolas Casamen";
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public NCAboutViewModel()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(MoreInfoUrl);
+    }
+}
